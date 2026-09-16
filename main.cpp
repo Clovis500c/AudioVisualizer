a0c92d439b937e74d54c090756a897acbf3adb7e
clovis500c <clovis500c@DESKTOP-FGMIKJM>
2026-09-16 21:24:25 +0200

    base

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <raylib.h>
+
+// Window Config //
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define WINDOW_FPS 120
+#define WINDOW_TITLE "Audio Visualizer"
 
 int main() {
-	printf("Hello, World!");
+	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
+
+	SetTargetFPS(WINDOW_FPS);
+
+	if (!WindowShouldClose()) {
+		
+	}
 
 	return 0;
 }
